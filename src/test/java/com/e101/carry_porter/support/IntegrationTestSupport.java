b40f8b1cbbaf487740c6470fc7f42a3d48fa2bd1
joonwan1024 <joownan1024@naver.com>
2026-06-09 23:33:32 +0900

    chore: db test 설정

---
+++ src/test/java/com/e101/carry_porter/support/IntegrationTestSupport.java
@@ -0,0 +1,31 @@
+package com.e101.carry_porter.support;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Transactional
+@Testcontainers
+@ActiveProfiles("test")
+public abstract class IntegrationTestSupport {
+
+    @Container
+    protected static final MySQLContainer<?> MYSQL_CONTAINER =
+            new MySQLContainer<>("mysql:8.4")
+                    .withDatabaseName("carry_porter_test")
+                    .withUsername("root")
+                    .withPassword("1234");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+    }
+}
