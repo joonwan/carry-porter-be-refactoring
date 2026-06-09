3d1e38cd3740adbb042ed0827c4754164a693286
joonwan <joonwan1024@naver.com>
2026-06-09 23:04:36 +0900

    feat: create new project

---
+++ src/main/java/com/e101/carry_porter/CarryPorterApplication.java
@@ -0,0 +1,13 @@
+package com.e101.carry_porter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarryPorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarryPorterApplication.class, args);
+	}
+
+}
