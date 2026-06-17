0dec94b4f843b0b70ac826320aec521f3a9a4d82
joonwan <joonwan1024@naver.com>
2026-06-17 16:55:15 +0900

    feat: robot offline 상태 추가

--- src/main/java/com/e101/carry_porter/domain/robot/entity/RobotStatus.java
+++ src/main/java/com/e101/carry_porter/domain/robot/entity/RobotStatus.java
@@ -1,6 +1,7 @@
 package com.e101.carry_porter.domain.robot.entity;
 
 public enum RobotStatus {
-    IDLE, // 배차 가능한 대기 상태
-    BUSY  // 미션 수행 또는 복귀 중으로 배차가 불가능한 상태
+    OFFLINE, // 서버와 연결되지 않아 배차할 수 없는 상태
+    IDLE,    // 서버와 연결되어 있으며 배차 가능한 대기 상태
+    BUSY     // 미션 수행 또는 복귀 중으로 배차가 불가능한 상태
 }
