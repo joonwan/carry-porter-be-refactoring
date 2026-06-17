23a693df14c978399b062874b77ec704c2d826e4
joonwan <joonwan1024@naver.com>
2026-06-17 16:59:13 +0900

    feat: 로봇 접속시 RobotConnectedMessageReceivedEvent 발행하도록 구현

---
+++ src/main/java/com/e101/carry_porter/domain/robot/event/RobotConnectedMessageReceivedEvent.java
@@ -0,0 +1,6 @@
+package com.e101.carry_porter.domain.robot.event;
+
+public record RobotConnectedMessageReceivedEvent(
+        String macAddress
+) {
+}
---
+++ src/main/java/com/e101/carry_porter/global/mqtt/serviceactivator/RobotConnectedServiceActivator.java
@@ -0,0 +1,32 @@
+package com.e101.carry_porter.global.mqtt.serviceactivator;
+
+import com.e101.carry_porter.domain.robot.event.RobotConnectedMessageReceivedEvent;
+import com.e101.carry_porter.domain.robot.mqtt.RobotInboundMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "carry-porter.mqtt", name = "enabled", havingValue = "true")
+public class RobotConnectedServiceActivator {
+
+    private final ApplicationEventPublisher eventPublisher;
+
+    @ServiceActivator(inputChannel = "robotConnectedChannel")
+    public void handle(Message<RobotInboundMessage> message) {
+        RobotInboundMessage inboundMessage = message.getPayload();
+
+        eventPublisher.publishEvent(new RobotConnectedMessageReceivedEvent(
+                inboundMessage.macAddress()
+        ));
+
+        log.info("RobotConnectedMessageReceivedEvent 발행: robotMacAddress = {}",
+                inboundMessage.macAddress());
+    }
+}
