c56cefb1c19704504f3d0d41be781b074ed9e911
joonwan <joonwan1024@naver.com>
2026-06-21 02:10:39 +0900

    test: 공용 클래스에 redis container 설정

--- src/test/java/com/e101/carry_porter/support/IntegrationTestSupport.java
+++ src/test/java/com/e101/carry_porter/support/IntegrationTestSupport.java
@@ -2,13 +2,14 @@ package com.e101.carry_porter.support;
 
 import com.e101.carry_porter.global.mqtt.MqttGateway;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.event.RecordApplicationEvents;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -16,6 +17,7 @@ import org.testcontainers.containers.MySQLContainer;
 public abstract class IntegrationTestSupport {
 
     protected static final MySQLContainer<?> MYSQL_CONTAINER;
+    protected static final GenericContainer<?> REDIS_CONTAINER;
 
     @MockitoBean
     private MqttGateway mqttGateway;
@@ -26,7 +28,11 @@ public abstract class IntegrationTestSupport {
                 .withUsername("root")
                 .withPassword("1234");
 
+        REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:7-alpine"))
+                .withExposedPorts(6379);
+
         MYSQL_CONTAINER.start();
+        REDIS_CONTAINER.start();
     }
 
     @DynamicPropertySource
@@ -34,5 +40,7 @@ public abstract class IntegrationTestSupport {
         registry.add("spring.datasource.url", MYSQL_CONTAINER::getJdbcUrl);
         registry.add("spring.datasource.username", MYSQL_CONTAINER::getUsername);
         registry.add("spring.datasource.password", MYSQL_CONTAINER::getPassword);
+        registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+        registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379));
     }
 }
